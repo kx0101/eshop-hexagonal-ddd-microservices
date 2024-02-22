6b561c4569efc5cd5fb84dfaae4a13f2296db96b
kx0101 <liakos.koulaxis@yahoo.com>
2024-02-22 23:02:51 +0200

    setup

--- customers/customers-app/src/test/java/com/elijahkx/customers/CustomersApplicationTests.java
+++ customers/customers-app/src/test/java/com/elijahkx/customers/CustomersApplicationTests.java
@@ -1,10 +1,10 @@
-package com.elijahkx.eshop;
+package com.elijahkx.customers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EshopApplicationTests {
+class CustomersApplicationTests {
 
 	@Test
 	void contextLoads() {
