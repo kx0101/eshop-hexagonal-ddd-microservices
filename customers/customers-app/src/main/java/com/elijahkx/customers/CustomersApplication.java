af2bbd8414e6cc98604e0c5a8eec4fcc2d138836
Elijah Koulaxis <liakos.koulaxis@yahoo.com>
2024-02-25 15:34:07 +0200

    .

--- customers/customers-app/src/main/java/com/elijahkx/customers/CustomersApplication.java
+++ customers/customers-app/src/main/java/com/elijahkx/customers/CustomersApplication.java
@@ -2,8 +2,12 @@ package com.elijahkx.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.elijahkx.exceptions.GlobalExceptionHandler;
 
 @SpringBootApplication
+@Import(GlobalExceptionHandler.class)
 public class CustomersApplication {
 
     public static void main(String[] args) {
