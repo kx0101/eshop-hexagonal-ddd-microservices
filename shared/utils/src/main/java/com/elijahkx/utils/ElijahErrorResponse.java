df66de3a35cbd48f77509cfe9ca27466131ad024
Elijah Koulaxis <liakos.koulaxis@yahoo.com>
2024-02-26 23:28:04 +0200

    .

--- shared/utils/src/main/java/com/elijahkx/utils/ElijahErrorResponse.java
+++ shared/utils/src/main/java/com/elijahkx/utils/ElijahErrorResponse.java
@@ -13,37 +13,10 @@ import lombok.Data;
 @AllArgsConstructor
 public class ElijahErrorResponse {
     private LocalDateTime timestamp;
+
     private int status;
+
     private String error;
+
     private List<String> messages;
-
-    public ElijahErrorResponse(HttpStatus status, List<String> messages) {
-        this.timestamp = LocalDateTime.now();
-        this.status = status.value();
-        this.error = status.getReasonPhrase();
-        this.messages = messages;
-    }
-
-    public ElijahErrorResponse(HttpStatus status, String message) {
-        this.timestamp = LocalDateTime.now();
-        this.status = status.value();
-        this.error = status.getReasonPhrase();
-        this.messages = Collections.singletonList(message);
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public List<String> getMessages() {
-        return messages;
-    }
 }
