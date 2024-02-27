a54fe11183a3faa1f1e89eaf5865d2963fbfd0b9
Elijah Koulaxis <liakos.koulaxis@yahoo.com>
2024-02-27 12:50:42 +0200

    .

--- orders/orders-app/src/main/java/com/elijahkx/orders/OrdersApplication.java
+++ orders/orders-app/src/main/java/com/elijahkx/orders/OrdersApplication.java
@@ -2,16 +2,21 @@ package com.elijahkx.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.elijahkx.exceptions.GlobalExceptionHandler;
 
 @SpringBootApplication
 @Import(GlobalExceptionHandler.class)
+@EnableFeignClients(basePackages = {
+        "com.elijahkx.customers.adapters.outbound.rest",
+})
 public class OrdersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrdersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class, args);
+    }
 
 }
